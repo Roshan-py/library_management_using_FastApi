ac028d693dedc5112f9bc67ce2154ac812b95241
roshan yadav <er.roshan.y@gmail.com>
2024-07-19 12:24:55 +0530

    changed sqlconnection for heroku

--- app/database.py
+++ app/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE = 'mysql+pymysql://root:Admin%40123@localhost:3306/library_management'
+# URL_DATABASE = 'mysql+pymysql://root:Admin%40123@localhost:3306/library_management'
 
 
-engine = create_engine(URL_DATABASE)
-
+# engine = create_engine(URL_DATABASE)
+engine = create_engine(f"sqlite:///database.db", echo=False, hide_parameters=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base = declarative_base()

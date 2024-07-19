c0b7a9ed1f3b43b9068c83e766bdbbb9fd25e2b8
roshan yadav <er.roshan.y@gmail.com>
2024-07-19 18:49:43 +0530

    Added middleware

--- server.py
+++ server.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.database import engine, Base
 from app.routers import books, users
+from fastapi.middleware.cors import CORSMiddleware
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(books.router, prefix="/books", tags=["books"])
